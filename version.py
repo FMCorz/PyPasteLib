7a3dc2e29408997eb0c0c19997732bea5d6bf768
Fred <fmcell@gmail.com>
2013-02-05 22:17:15 +0800

    Defining version 1.0

---
+++ version.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+PyPasteLib
+
+A Python library to format and paste text.
+
+Copyright (c) 2013 Frédéric Massart - FMCorz.net
+
+Licensed under The MIT License
+Redistributions of files must retain the above copyright notice.
+
+http://github.com/FMCorz/PyPasteLib
+"""
+
+__version__ = '1.0'
